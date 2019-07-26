8397a4782e96751777649023f3c14e08e17376a0
Thomas Krause <thomaskrause@posteo.de>
2019-07-26 11:12:35 +0200

    Initial commit (only includes a test call to epic_disp_print)

---
+++ src/display.rs
@@ -0,0 +1,11 @@
+use super::*;
+
+pub fn print(posx : u16, posy : u16, p_string :  &str, fg : Color, bg : Color) -> Result<()> {
+    unsafe {
+        let result = epic_disp_print(posx, posy, p_string.as_ptr(), fg.rgb565(), bg.rgb565());
+        if result != 0 {
+            return Err(Error::EBUSY);
+        }
+    }
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+pub mod display;
+
+#[link(name="api-caller", kind="static")]
+#[link(name="PeriphDriver", kind="static")]
+extern "C" {
+    fn epic_disp_print(posx : u16, posy : u16, pString :  *const u8, fg : u16, bg : u16) -> i32;
+}
+
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Color {
+    fn rgb565(self) -> u16 {
+        let r5 = ((self.r as u16) >> 3) as u8;
+        let g6 = ((self.g as u16) >> 2) as u8;
+        let b5 = ((self.b as u16) >> 3) as u8;
+
+        let result1 = ((g6 & 0b000111) << 5) | b5;
+        let result2 = (r5 << 3) | ((g6 & 0b111000) >> 3);
+
+        ((result1 as u16) << 8) | (result2 as u16)
+    }
+}
+
+
+#[derive(Debug)]
+pub enum Error {
+    EBUSY
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
