59fc3451fffad3dcfb1ac48a53e53cb546a1d9e3
Thomas Krause <thomaskrause@posteo.de>
2019-08-20 21:38:56 +0200

    Force the user to set the environment variable EPICARDIUM_BUILD_DIR as an absolute path to the output directory

--- epicardium-sys/build.rs
+++ epicardium-sys/build.rs
@@ -17,9 +17,12 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings to epicardium.h!");
+  
+    let firmware_build_dir = env::var("EPICARDIUM_BUILD_DIR").expect("You need to set the EPICARDIUM_BUILD_DIR environment variable to the build directory of the firmware");
 
-
-    println!("cargo:rustc-link-search=ext/");
+    println!("cargo:rustc-link-search={}/epicardium", firmware_build_dir);
+    println!("cargo:rustc-link-search={}/lib/sdk/Libraries/MAX32665PeriphDriver", firmware_build_dir);
+    
     println!("cargo:rustc-link-lib=static=PeriphDriver");
     println!("cargo:rustc-link-lib=static=api-caller");
 
